1ca0d0acf849cbae465c7a2fc3a9b386df231089
suzukaotto <me@szk.kr>
2024-09-23 19:12:26 +0900

    update: VERSION update to 2.0

--- updater.py
+++ updater.py
@@ -5,6 +5,8 @@ git_version_doc = requests.get('https://github.com/suzukaotto/version-test/blob/
 
 match = re.search(r'\[SWVERSION\](.*?)\[SWVERSIONEND\]', git_version_doc)
 
+result = None
 if match:
     result = match.group(1)
-    print(result)
+
+print(result)
