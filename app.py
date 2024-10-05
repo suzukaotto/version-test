945258a6bcb9cf3f2f08073491879030bb83d7d0
suzukaotto <me@szk.kr>
2024-10-05 21:17:03 +0900

    update: app.py update

--- app.py
+++ app.py
@@ -1,4 +1,16 @@
-with open("./VERSION", "r") as f:
-    VERSION = f.read().strip()
+import requests
+from updater import Updater
 
-print(f"Hello, now version is {VERSION}")
+AppUpdater = Updater()
+AppUpdater.set_check_url("https://github.com/suzukaotto/version-test/blob/master/VERSION")
+CLOUD_VERSION = AppUpdater.get_now_version()
+
+with open("./VERSION", "r") as f:
+    VERSION = AppUpdater.find_version(f.read().strip())
+
+print(f"Hello, now version is {VERSION}, cloud version is {CLOUD_VERSION}")
+
+if VERSION < CLOUD_VERSION:
+    print("Update available!")
+else:
+    print("Your lasted version!")
