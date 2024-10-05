9cb8ed321c2080963eb0167acd66f91836cdd022
suzukaotto <me@szk.kr>
2024-10-05 21:16:44 +0900

    update: updater class upgraded

--- updater.py
+++ updater.py
@@ -1,12 +1,32 @@
 import requests
 import re
 
-git_version_doc = requests.get('https://github.com/suzukaotto/version-test/blob/master/VERSION').text
-
-match = re.search(r'\[SWVERSION\](.*?)\[SWVERSIONEND\]', git_version_doc)
-
-result = None
-if match:
-    result = match.group(1)
-
-print(result)
+class Updater:
+    def __init__(self):
+        self.api_timeout = 5
+        self.check_url = None
+        
+    def set_check_url(self, url):
+        self.check_url = url
+        
+    def get_now_version(self):
+        if self.check_url is None:
+            return None
+        
+        try:
+            git_version_doc = requests.get(self.check_url, timeout=self.api_timeout).text
+        except:
+            return None
+        
+        return self.find_version(git_version_doc)
+    
+    def find_version(self, str, pattern=r'\[SWVERSION\](.*?)\[SWVERSIONEND\]'):
+        match = re.search(pattern, str)
+        
+        result = None
+        if match:
+            result = match.group(1)
+        
+        result = float(result)
+        
+        return result
